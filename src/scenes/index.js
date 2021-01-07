c388582931535ce35af3861c735202fca8087645
Yui-Ezic <mishan221199@gmail.com>
2021-01-07 16:02:53 +0200

    Disable 1st and second scenes

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -4,8 +4,8 @@ import { scene as Scene3 } from "./3/index";
 import { scene as Scene4 } from "./4/index";
 
 export const scenes = {
-    "/1": Scene1,
-    "/2": Scene2,
+    // "/1": Scene1,
+    // "/2": Scene2,
     "/3": Scene3,
     "/4": Scene4
 };
